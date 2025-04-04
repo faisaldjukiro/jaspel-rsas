ae56ef7383bfe8cf03b7eebebe2432c0dd424e59
Faisal Djukiro <faisaldjukiro98@gmai.com>
2025-04-04 21:21:35 +0800

    perbaiki setelah kena firus

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'jasa',
+	'hostname' => 'srv1364.hstgr.io',
+	'username' => 'u845682289_jaspel_faisal',
+	'password' => '@Politeknik02',
+	'database' => 'u845682289_jaspel',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
